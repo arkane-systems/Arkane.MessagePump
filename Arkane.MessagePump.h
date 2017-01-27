fd601b811a08e5101946e5f72d6407525732d298
Alistair Young <avatar@arkane-systems.net>
2017-01-27 07:54:12 -0600

    Add project files.

---
+++ Arkane.MessagePump.h
@@ -0,0 +1,13 @@
+// Arkane.MessagePump.h
+
+#pragma once
+
+using namespace System;
+
+namespace ArkaneMessagePump {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
---
+++ Stdafx.cpp
@@ -0,0 +1,5 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Arkane.MessagePump.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
